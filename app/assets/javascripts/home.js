b4a0335ddc00299e4436d32746e6914d0d28e321
dannyespina <dannyj.espina@gmail.com>
2017-02-28 01:37:15 +0000

    added inset box shodow for navBar

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
     $("#shopDrop").mouseenter(function(){
         $("#shopPanel").fadeIn("fast")
         $("#shopDisplay").fadeIn("fast")
+        $("#menu").addClass("menuShadow")
         $("#shopDrop").mouseleave(function(){
+            $("#menu").removeClass("menuShadow")
             $("#shopPanel").fadeOut("fast")
-             $("#shopDisplay").fadeOut("fast")
+            $("#shopDisplay").fadeOut("fast")
         });
         
    });
