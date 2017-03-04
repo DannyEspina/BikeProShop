73919b5fee39e46824852426eb8834df6ba80e9c
dannyespina <dannyj.espina@gmail.com>
2017-03-04 12:44:33 +0000

    created Logo. Maybe more in the future

--- app/assets/javascripts/googleMaps.js
+++ app/assets/javascripts/googleMaps.js
@@ -1,7 +1,7 @@
  function myMap() {
           var myCenter = new google.maps.LatLng(34.882067, -82.150943);
           var mapCanvas = document.getElementById("map");
-          var mapOptions = {center: myCenter, zoom: 15};
+          var mapOptions = {center: myCenter, zoom: 15, scrollwheel:false};
           var map = new google.maps.Map(mapCanvas, mapOptions);
           var marker = new google.maps.Marker({position:myCenter});
           marker.setMap(map);
