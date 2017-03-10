e26d444fdc6b335e533670c53059ebe6e3bfb990
dannyespina <dannyj.espina@gmail.com>
2017-03-10 03:21:00 +0000

    made the logo work on all screen sizes

--- app/assets/javascripts/googleMaps.js
+++
@@ -1,8 +0,0 @@
- function myMap() {
-          var myCenter = new google.maps.LatLng(34.882067, -82.150943);
-          var mapCanvas = document.getElementById("map");
-          var mapOptions = {center: myCenter, zoom: 15, scrollwheel:false};
-          var map = new google.maps.Map(mapCanvas, mapOptions);
-          var marker = new google.maps.Marker({position:myCenter});
-          marker.setMap(map);
-        }
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,3 +13,17 @@ $(document).ready(function(){
         
    });
 });
+
+//google maps
+ function myMap() {
+          var myCenter = new google.maps.LatLng(34.882067, -82.150943);
+          var mapCanvas = document.getElementById("map");
+          var mapOptions = {center: myCenter, zoom: 15, scrollwheel:false};
+          var map = new google.maps.Map(mapCanvas, mapOptions);
+          var marker = new google.maps.Marker({position:myCenter});
+          marker.setMap(map);
+        }
+    function myYoutube(){
+        var sparks = iframe.getElementById('sparks')
+        sparks.mute();
+    }
