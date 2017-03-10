d4b4002319e160aa010954b78352c2dfd0df8fb3
dannyespina <dannyj.espina@gmail.com>
2017-03-10 16:52:43 +0000

    fixed the size of 1st youtube video. working on parallax

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -21,7 +21,39 @@ $(document).ready(function(){
           var marker = new google.maps.Marker({position:myCenter});
           marker.setMap(map);
         }
-    function myYoutube(){
-        var sparks = iframe.getElementById('sparks')
-        sparks.mute();
-    }
+        
+
+// Loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replaces the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+       
+        videoId: 'kQ5QhgEPG8o',
+        playerVars: {
+          autoplay: 1, //automatically plays video when page loads
+          controls: 0, //removes controls
+          loop: 1,
+          modestbranding: 1, //remove youtube logo
+          showinfo: 0, //removes title
+          autohide: 1,
+          playlist: 'kQ5QhgEPG8o', //this is required for the loop to work
+          iv_load_policy: 3, // video annotations to not be shown by default
+          disablekb: 1, // disables keyboard controls
+          rel: 0 //don't show related videos
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+  }
+
+function onPlayerReady(event){
+    player.mute();
+}
