ccc2dcfd5d66a84ce745112081c8c25d8a0ed44e
dannyespina <dannyj.espina@gmail.com>
2017-03-29 22:16:21 +0000

    working on users

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/repair', to: 'repair#repair'
   get '/index', to: 'home#index'
   get '/gallery', to:'photos#gallery'
-  
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
