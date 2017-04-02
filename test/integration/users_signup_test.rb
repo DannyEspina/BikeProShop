8c5a6b98c868e79abcf19865c5ecad1db748bda5
dannyespina <dannyj.espina@gmail.com>
2017-04-02 01:05:47 +0000

    finished sign-up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger'
   end
+  
+  test "valid signup information" do
+    get new_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    
+  end
+  
 end
