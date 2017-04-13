8cb1f11904e62deb0359d559c341fccab6d1ee65
dannyespina <dannyj.espina@gmail.com>
2017-04-13 01:17:39 +0000

    trying to fix admin acc

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,3 @@ User.create!(name:  "Daniel Espina",
              password:              "Miamibikes307",
              password_confirmation: "Miamibikes307",
              admin: true)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
-
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
