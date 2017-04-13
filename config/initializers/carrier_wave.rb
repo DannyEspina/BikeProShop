e22ddb934b830eae3d727698b9bff43bfcc9b5b6
dannyespina <dannyj.espina@gmail.com>
2017-04-13 01:06:58 +0000

    trying to add s3 to heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAI2DDZMOWS4IUR3DA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['bikeproshop']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
