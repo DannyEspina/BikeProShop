ff96f771fe003046cfcf86852e8f90a384fd6b98
Daniel Espina <dannyj.espina@gmail.com>
2017-12-23 22:23:59 -0500

    Fixed bug where the delete button olny deletes the most recent posts in blog

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,12 +1,12 @@
 class MicropostsController < ApplicationController
      before_action :logged_in_user, only: [:create, :destroy, :index]
      before_action :correct_user,   only: :destroy
-     
+
   def index
     @micropost = current_user.microposts.build
     @feed_items = current_user.feed.paginate(page: params[:page])
   end
-  
+
   def create
         @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -17,22 +17,22 @@ class MicropostsController < ApplicationController
       render action: "index"
     end
   end
-  
+
   def destroy
-      @micropost.destroy
+    @micropost.destroy
     flash[:success] = "Micropost deleted"
     redirect_to request.referrer || microposts_path
   end
-  
+
    private
 
     def micropost_params
       params.require(:micropost).permit(:content, :picture)
     end
-    
+
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to microposts_path if @micropost.nil?
     end
-    
+
 end
