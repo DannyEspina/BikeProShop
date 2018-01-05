7136f75ffd64edfa9b6bedb96fc0a5e2fbcf2bc1
Daniel Espina <dannyj.espina@gmail.com>
2018-01-05 18:31:28 -0500

    Hopefully the last improvement to the bicycle scroll animation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -16,54 +16,57 @@ $(document).on('turbolinks:load', function() {
         at 790 pixels down. If the window width is samller than 991 pixels then
         don't do the animation. */
 
-      //  console.log("scroll: " + wScroll + "offset: " + $('.header').offset().top);
-        var offset = $('.header').offset().top;
+        console.log("scroll: " + wScroll + " offset: " + $('.header').offset().top);
+        // subtract 150 to start sooner.
+        var offset = $('.header').offset().top - 150;
         var max = offset + 340;
-        if (wScroll > 450 &&
-            wScroll <= 790) {
-            var start = wScroll - 450;
+
+        /*
+        As the user scroll down I want to move/translate the bibycle
+        to the mechanic on any screen sizes.
+
+        We first delcare the start variable which stores the scroll with
+        a offset of the distance of the .header class (The start of the animation)
+
+        Then we find the difference between the start and the end of the
+        scroll animation: 790-450 = 340
+
+        So the plan is for start to go from 1 - 340. We then have to
+        convert the start variable to a 1-100 scale and store it into
+        the percent variable: (start/340) * 100.
+
+        I discovered from playing with the CSS that it takes up to 26vw
+        for the bicycle to reach the mechanic. The vw unit is key for
+        this animation to work on different screen sizes.
+
+        So in order to convert percent to a vw scale we need to solve for
+        x in this equation:
+
+        percent / 100 = x / 26
+
+        which is
+
+        x = 26 * percent / 100
+
+        we store this into the move variable.
+
+        place the move variable into transform: translate(move,0)
+
+        ta-da! The bicycle moves to customer to mechanic on any screen
+        size.
+
+        we can simply this by removing the 100 in the percent and move
+        variables:
+
+        var percent = (start / 340) * 100 => start / 340
+        var move = (percent * 26) / 100 => percent * 26
+        */
+        if (wScroll > offset &&
+            wScroll <= max) {
+            var start = wScroll - offset;
             var percent = start / 340;
             var move = percent * 26
-            /*
-            As the user scroll down I want to move/translate the bibycle
-            to the mechanic on any screen sizes.
 
-            We first delcare the start variable which stores the scroll with
-            a offset of 450 (The start of the animation)
-
-            Then we find the difference between the start and the end of the
-            scroll animation: 790-450 = 340
-
-            So the plan is for start to go from 1 - 340. We then have to
-            convert the start variable to a 1-100 scale and store it into
-            the percent variable: (start/340) * 100.
-
-            I discovered from playing with the CSS that it takes up to 26vw
-            for the bicycle to reach the mechanic. The vw unit is key for
-            this animation to work on different screen sizes.
-
-            So in order to convert percent to a vw scale we need to solve for
-            x in this equation:
-
-            percent / 100 = x / 26
-
-            which is
-
-            x = 26 * percent / 100
-
-            we store this into the move variable.
-
-            place the move variable into transform: translate(move,0)
-
-            ta-da! The bicycle moves to customer to mechanic on any screen
-            size.
-
-            we can simply this by removing the 100 in the percent and move
-            variables:
-
-            var percent = (start / 340) * 100 => start / 340
-            var move = (percent * 26) / 100 => percent * 26
-            */
           $('.customBicycle').css({
               'transform': 'translate( '+move+'vw, 0px)'
           });
