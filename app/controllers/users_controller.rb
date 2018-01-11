39040f94864a11f946abdbe16dc329beef056b44
Daniel Espina <dannyj.espina@gmail.com>
2018-01-11 00:01:28 -0500

    fixed problem with login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,28 @@
 class UsersController < ApplicationController
-  
+
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-  
+
   def index
     @users = User.paginate(page: params[:page])
   end
-  
+
   def new
      @user = User.new
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
-  
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
     end
-    
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -33,11 +33,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
   end
-  
-  
+
+
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
@@ -46,26 +46,26 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-    
-    
+
+
     # Before filters
-    
+
       # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
-    
+
     # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
-    
+
 end
