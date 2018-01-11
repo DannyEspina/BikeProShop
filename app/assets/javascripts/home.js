ef445f92ddeb92901359a4501f3f19a11ddbb424
Daniel Espina <dannyj.espina@gmail.com>
2018-01-10 21:59:12 -0500

    fixed google maps bug

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -63,47 +63,47 @@ $(document).on('turbolinks:load', function() {
         the map won't load if you go to a direct page and don't refresh.
         we need it to be inside $(document).on('turbolinks:load', function()
         */
-      //   if (this.hash !== "" && location.pathname == "/" ) {
-      //     myMap();
-      // }
+        if (this.hash !== "" && location.pathname == "/" ) {
+          myMap();
+      }
 });
 //google maps
-// function myMap() {
-//     //centers the map to the adress of the barnyard flea market
-//     var myCenter = new google.maps.LatLng(34.882067, -82.150943);
-//     //selects element to place the map which in this case is the map div
-//     var mapCanvas = document.getElementById("map");
-//     //adds the options
-//     var mapOptions = {
-//         center: myCenter,
-//         zoom: 15,
-//         scrollwheel: false,
-//         styles: [
-//   {
-//     "featureType": "administrative.country",
-//     "elementType": "geometry.fill",
-//     "stylers": [
-//       {
-//         "visibility": "on"
-//       }
-//     ]
-//   },
-//   {
-//     "featureType": "landscape.natural",
-//     "elementType": "geometry.fill",
-//     "stylers": [
-//       {
-//         "color": "#f3f2f2"
-//       }
-//     ]
-//   }
-// ]
-//     };
-//     //creates the map
-//     var map = new google.maps.Map(mapCanvas, mapOptions);
-//     //places the marker on the barnyard flea market
-//     var marker = new google.maps.Marker({
-//         position: myCenter
-//     });
-//     marker.setMap(map);
-// }
+function myMap() {
+    //centers the map to the adress of the barnyard flea market
+    var myCenter = new google.maps.LatLng(34.882067, -82.150943);
+    //selects element to place the map which in this case is the map div
+    var mapCanvas = document.getElementById("map");
+    //adds the options
+    var mapOptions = {
+        center: myCenter,
+        zoom: 15,
+        scrollwheel: false,
+        styles: [
+  {
+    "featureType": "administrative.country",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#f3f2f2"
+      }
+    ]
+  }
+]
+    };
+    //creates the map
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    //places the marker on the barnyard flea market
+    var marker = new google.maps.Marker({
+        position: myCenter
+    });
+    marker.setMap(map);
+}
